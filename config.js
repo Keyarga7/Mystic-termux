c0d700deabce68ee8f64b6800cd778375a669e20
Keyarga7 <131921725+Keyarga7@users.noreply.github.com>
2023-04-26 17:26:09 -0600

    Update config.js

--- config.js
+++ config.js
@@ -8,19 +8,19 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['525560066050', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],  
-  ['5212411719888', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215533827255', '💫 Mystic - Collaborator 7 💫', false],
-  ['593968585283'],
-  ['5219993404349'],
-  ['584125778026'],
-  ['5219991402134'],	
-  ['5219996125657']
+  ['525647084779', '👑 Mystic - Creador 👑', true],
+  ['522381902796', '💫 Mystic - Collaborator 1 💫', true],
+  ['522381697271', '💫 Mystic - Collaborator 2 💫', true],
+  ['', '💫 Mystic - Collaborator 3 💫', true],
+  ['', '💫 Mystic - Collaborator 4 💫', true],
+  ['', '💫 Mystic - Collaborator 5 💫', true],  
+  ['', '💫 Mystic - Collaborator 6 💫', true],
+  ['', '💫 Mystic - Collaborator 7 💫', false],
+  [''],
+  [''],
+  [''],
+  [''],	
+  ['']
 ] 
 
 global.suittag = ['5219993404349'] 
